85e125f8c07412d3239b1adc7b3edeb9fa36ab5e
Zack Slayton <zack.slayton@gmail.com>
2015-05-29 12:46:51 -0400

    Test parity for Recycled and RcRecycled. Fixes #6.

--- tests/lib.rs
+++ tests/lib.rs
@@ -7,6 +7,13 @@ mod tests {
   #[test]
   fn test_deref() {
       let str_pool : Pool<String> = Pool::with_size(1);
+      let rstring = str_pool.new_from("cat");
+      assert_eq!("cat", *rstring);
+  }
+
+  #[test]
+  fn test_deref_rc() {
+      let str_pool : Pool<String> = Pool::with_size(1);
       let rstring = str_pool.new_rc_from("cat");
       assert_eq!("cat", *rstring);
   }
@@ -14,14 +21,29 @@ mod tests {
   #[test]
   fn test_deref_mut() {
       let str_pool : Pool<String> = Pool::with_size(1);
+      let mut rstring = str_pool.new_from("cat");
+      rstring.push_str("s love eating mice");
+      assert_eq!("cats love eating mice", *rstring);
+  }
+
+  #[test]
+  fn test_deref_mut_rc() {
+      let str_pool : Pool<String> = Pool::with_size(1);
       let mut rstring = str_pool.new_rc_from("cat");
       rstring.push_str("s love eating mice");
-      println!("{}", *rstring);
       assert_eq!("cats love eating mice", *rstring);
   }
 
   #[test]
   fn test_as_mut() {
+      let str_pool : Pool<String> = Pool::with_size(1);
+      let mut rstring = str_pool.new_from("cat");
+      rstring.as_mut().push_str("s love eating mice");
+      assert_eq!("cats love eating mice", *rstring);
+  }
+
+  #[test]
+  fn test_as_mut_rc() {
       let str_pool : Pool<String> = Pool::with_size(1);
       let mut rstring = str_pool.new_rc_from("cat");
       rstring.as_mut().push_str("s love eating mice");
@@ -30,6 +52,13 @@ mod tests {
 
   #[test]
   fn test_as_ref() {
+      let str_pool : Pool<String> = Pool::with_size(1);
+      let rstring = str_pool.new_from("cat");
+      assert_eq!("cat", rstring.as_ref());
+  }
+
+  #[test]
+  fn test_as_ref_rc() {
       let str_pool : Pool<String> = Pool::with_size(1);
       let rstring = str_pool.new_rc_from("cat");
       assert_eq!("cat", rstring.as_ref());
@@ -40,6 +69,17 @@ mod tests {
       let str_pool : Pool<String> = Pool::with_size(1);
       {
         assert_eq!(1, str_pool.size());
+        let _rstring = str_pool.new_from("cat");
+        assert_eq!(0, str_pool.size());
+      }
+      assert_eq!(1, str_pool.size());
+  }
+
+  #[test]
+  fn test_recycle_rc() {
+      let str_pool : Pool<String> = Pool::with_size(1);
+      {
+        assert_eq!(1, str_pool.size());
         let _rstring = str_pool.new_rc_from("cat");
         assert_eq!(0, str_pool.size());
       }
@@ -47,7 +87,18 @@ mod tests {
   }
 
   #[test]
-  fn test_detached() {
+  fn test_detach() {
+      let str_pool : Pool<String> = Pool::with_size(1);
+      {
+        assert_eq!(1, str_pool.size());
+        let _string : String = str_pool.new().detach();
+        assert_eq!(0, str_pool.size());
+      }
+      assert_eq!(0, str_pool.size());
+  }
+
+  #[test]
+  fn test_detach_rc() {
       let str_pool : Pool<String> = Pool::with_size(1);
       {
         assert_eq!(1, str_pool.size());
@@ -57,17 +108,6 @@ mod tests {
       assert_eq!(0, str_pool.size());
   }
 
-  #[test]
-  fn test_attach_rc() {
-      let str_pool : Pool<String> = Pool::with_size(1);
-      {
-        assert_eq!(1, str_pool.size());
-        let string: String = str_pool.new_rc().detach();
-        assert_eq!(0, str_pool.size());
-        let _rstring: RcRecycled<String> = str_pool.attach_rc(string);
-      }
-      assert_eq!(1, str_pool.size());
-  }
   #[test]
   fn test_attach() {
       let str_pool : Pool<String> = Pool::with_size(1);
@@ -79,14 +119,15 @@ mod tests {
       }
       assert_eq!(1, str_pool.size());
   }
+
   #[test]
-  fn test_attach_from_rc() {
+  fn test_attach_rc() {
       let str_pool : Pool<String> = Pool::with_size(1);
       {
         assert_eq!(1, str_pool.size());
-        let string: String = str_pool.new().detach();
+        let string: String = str_pool.new_rc().detach();
         assert_eq!(0, str_pool.size());
-        let _rstring: Recycled<String> = str_pool.attach(string);
+        let _rstring: RcRecycled<String> = str_pool.attach_rc(string);
       }
       assert_eq!(1, str_pool.size());
   }
