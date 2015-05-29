07079b6f5b2218276eb34b200bcff1a5497e93fc
Zack Slayton <zack.slayton@gmail.com>
2015-05-29 13:28:09 -0400

    Benchmark parity for Recycled and RcRecycled.

--- benches/lib.rs
+++ benches/lib.rs
@@ -6,7 +6,7 @@ extern crate lifeguard;
 mod tests {
   use test::Bencher;
   use test::black_box;
-  use lifeguard::{Pool,RcRecycled};
+  use lifeguard::{Pool,Recycled,RcRecycled};
 
   const ITERATIONS : u32 = 10_000;
 
@@ -16,7 +16,7 @@ mod tests {
   const EMPTY_STRING_CAPACITY : usize = 4;
 
   #[bench]
-  fn bench01_standard_allocation_speed(b: &mut Bencher) {
+  fn allocation_standard(b: &mut Bencher) {
     b.iter(|| {
       for _ in 0..ITERATIONS {
         let _string = black_box(String::with_capacity(EMPTY_STRING_CAPACITY));
@@ -29,21 +29,7 @@ mod tests {
   }
 
   #[bench]
-  fn bench02_pooled_allocation_speed_rc(b: &mut Bencher) {
-    let pool : Pool<String> = Pool::with_size(5);
-    b.iter(|| {
-      for _ in 0..ITERATIONS {
-        let _string = pool.new_rc();
-        let _string = pool.new_rc();
-        let _string = pool.new_rc();
-        let _string = pool.new_rc();
-        let _string = pool.new_rc();
-      }
-    });
-  }
-
-  #[bench]
-  fn bench02_pooled_allocation_speed_scoped(b: &mut Bencher) {
+  fn allocation_pooled(b: &mut Bencher) {
     let pool : Pool<String> = Pool::with_size(5);
     b.iter(|| {
       for _ in 0..ITERATIONS {
@@ -57,7 +43,21 @@ mod tests {
   }
 
   #[bench]
-  fn bench03_standard_initialized_allocation_speed(b: &mut Bencher) {
+  fn allocation_pooled_rc(b: &mut Bencher) {
+    let pool : Pool<String> = Pool::with_size(5);
+    b.iter(|| {
+      for _ in 0..ITERATIONS {
+        let _string = pool.new_rc();
+        let _string = pool.new_rc();
+        let _string = pool.new_rc();
+        let _string = pool.new_rc();
+        let _string = pool.new_rc();
+      }
+    });
+  }
+
+  #[bench]
+  fn initialized_allocation_standard(b: &mut Bencher) {
     b.iter(|| {
       for _ in 0..ITERATIONS {
         let _string = "man".to_owned();
@@ -70,7 +70,21 @@ mod tests {
   }
 
   #[bench]
-  fn bench04_pooled_initialized_allocation_speed(b: &mut Bencher) {
+  fn initialized_allocation_pooled(b: &mut Bencher) {
+    let pool : Pool<String> = Pool::with_size(5);
+    b.iter(|| {
+      for _ in 0..ITERATIONS {
+        let _string = pool.new_from("man");
+        let _string = pool.new_from("dog");
+        let _string = pool.new_from("cat");
+        let _string = pool.new_from("mouse");
+        let _string = pool.new_from("cheese");
+      }
+    });
+  }
+
+  #[bench]
+  fn initialized_allocation_pooled_rc(b: &mut Bencher) {
     let pool : Pool<String> = Pool::with_size(5);
     b.iter(|| {
       for _ in 0..ITERATIONS {
@@ -82,8 +96,9 @@ mod tests {
       }
     });
   }
+
   #[bench]
-  fn bench05_allocate_vec_vec_str(bencher: &mut Bencher) {
+  fn vec_vec_str_standard(bencher: &mut Bencher) {
       bencher.iter(|| {
           let mut v1 = Vec::new();
           for _ in 0..100 {
@@ -98,7 +113,28 @@ mod tests {
   }
 
   #[bench]
-  fn bench06_pooled_vec_vec_str(bencher: &mut Bencher) {
+  fn vec_vec_str_pooled(bencher: &mut Bencher) {
+      // Note that because we're using scoped values (not Rc'ed values)
+      // and we're storing items from one pool in the other,
+      // the order that our pools are declared matters. 
+      // Reversing them results in a compile error regarding lifetimes.
+      let str_pool : Pool<String> = Pool::with_size(10000);
+      let vec_str_pool : Pool<Vec<Recycled<String>>> = Pool::with_size(100);
+      bencher.iter(|| {
+          let mut v1 = Vec::new();
+          for _ in 0..100 {
+              let mut v2 = vec_str_pool.new();
+              for _ in 0..100 {
+                  v2.push(str_pool.new_from("test!"));
+              }
+              v1.push(v2);
+          }
+          v1
+      });
+  }
+
+  #[bench]
+  fn vec_vec_str_pooled_rc(bencher: &mut Bencher) {
       let vec_str_pool : Pool<Vec<RcRecycled<String>>> = Pool::with_size(100);
       let str_pool : Pool<String> = Pool::with_size(10000);
       bencher.iter(|| {
